125285cfb7f41050e264dcc3bbd0c30b71cecda8
Ricardo Martins <ricardo@scarybox.net>
2011-06-08 19:44:25 +0100

    Added header.

--- du_sort.py
+++ du_sort.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python
+"""
+du_sort
+
+Copyright (c) 2011 Ricardo Martins
+Licensed under the MIT License.
+http://www.opensource.org/licenses/mit-license.php
+
+
+Don't you hate when you want to quickly check what's using the most space in
+your laptop's disk or server's RAID but 'du -hsc *' gives you unsorted output?
+
+Using 'du -hsc *| sort --numeric-sort' doesn't help because the units after the
+size mess everything up and using 'du -k' to display the size in blocks would
+allow easy sorting but defeat the purpose of having human-readable units.
+
+So I did what any respectable programmer would do: create a script that would
+sort the results exactly as I want them. That's how this script was born.
+
+I hope it's as useful to you as it has been to me. :)
+
+"""
 
 import sys
 
