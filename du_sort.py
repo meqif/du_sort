0e8f6ff4ae427e47d7ec96d317bca73e913b574d
Ricardo Martins <ricardo@scarybox.net>
2011-06-09 11:07:45 +0100

    More styles fixes according to Tornado's commenting style.

--- du_sort.py
+++ du_sort.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
-"""
-du_sort
+#
+# Copyright (c) 2011 Ricardo Martins
+#
+# Licensed under the MIT License.
+# http://www.opensource.org/licenses/mit-license.php
 
-Copyright (c) 2011 Ricardo Martins
-Licensed under the MIT License.
-http://www.opensource.org/licenses/mit-license.php
-
+""" du_sort: a short script for sorting 'du' output correctly
 
 Don't you hate when you want to quickly check what's using the most space in
 your laptop's disk or server's RAID but 'du -hsc *' gives you unsorted output?
 
-Using 'du -hsc *| sort --numeric-sort' doesn't help because the units after the
+Using 'du -hsc * | sort --numeric-sort' doesn't help because the units after the
 size mess everything up and using 'du -k' to display the size in blocks would
 allow easy sorting but defeat the purpose of having human-readable units.
 
@@ -18,17 +18,15 @@ So I did what any respectable programmer would do: create a script that would
 sort the results exactly as I want them. That's how this script was born.
 
 I hope it's as useful to you as it has been to me. :)
-
 """
 
 import sys
 
 def sort_criterion(line):
-    """
-    Return the size in bytes or adimensional units of a 'du' output line.
+    """ Returns the size in bytes or adimensional units of a 'du' output line.
 
-    Defines the sort criterion for 'du' lines in the form 'size filename',
-    where size may be adimensional.
+    Defines the sort criterion for 'du' lines in the form 'size filename', where
+    size may be adimensional.
 
     >>> sort_criterion("4,0K\tLICENSE")
     4096.0
